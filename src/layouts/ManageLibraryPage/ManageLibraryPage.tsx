7a51ef6c8dadededa8fb6f6f58ed731bf614882d
Slackjaw1431 <55801287+Slackjaw1431@users.noreply.github.com>
2023-05-08 20:03:06 -0400

    Add ManageLibraryPage.tsx

---
+++ src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
@@ -0,0 +1,73 @@
+import {useState} from "react";
+import {useOktaAuth} from "@okta/okta-react";
+import {Redirect} from "react-router-dom";
+
+export const ManageLibraryPage = () => {
+
+    const { authState } = useOktaAuth();
+
+    const [changeQuantityOfBooks, setChangeQuantityOfBooks] = useState(false);
+    const [messagesClick, setMessagesClick] = useState(false);
+
+    function addBookClickFunction() {
+        setChangeQuantityOfBooks(false);
+        setMessagesClick(false);
+    }
+
+    function changeQuantityOfBooksClickFunction() {
+        setChangeQuantityOfBooks(true);
+        setMessagesClick(false);
+    }
+
+    function messagesClickFunction() {
+        setChangeQuantityOfBooks(false);
+        setMessagesClick(true);
+    }
+
+    if (authState?.accessToken?.claims.userType === undefined) {
+        return <Redirect to='/home'/>
+    }
+
+
+    return(
+        <div className='container'>
+            <div className='mt-5'>
+                <h3>Manage Library</h3>
+                <nav>
+                    <div className='nav nav-tabs' id='nav-tab' role='tablist'>
+                        <button onClick={addBookClickFunction} className='nav-link active' id='nav-add-book-tab' data-bs-toggle='tab'
+                                data-bs-target='#nav-add-book' type='button' role='tab' aria-controls='nav-add-book'
+                                aria-selected='false'
+                        >
+                            Add new book
+                        </button>
+                        <button onClick={changeQuantityOfBooksClickFunction} className='nav-link' id='nav-quantity-tab' data-bs-toggle='tab'
+                                data-bs-target='#nav-quantity' type='button' role='tab' aria-controls='nav-quantity'
+                                aria-selected='true'
+                        >
+                            Change quantity
+                        </button>
+                        <button onClick={messagesClickFunction} className='nav-link' id='nav-messages-tab' data-bs-toggle='tab'
+                                data-bs-target='#nav-messages' type='button' role='tab' aria-controls='nav-messages'
+                                aria-selected='false'
+                        >
+                            Messages
+                        </button>
+                    </div>
+                </nav>
+                <div className='tab-content' id='nav-tabContent'>
+                    <div className='tab-pane fade show active' id='nav-add-book' role='tabpanel'
+                         aria-labelledby='nav-add-book-tab'>
+                        Add New Book
+                    </div>
+                    <div className='tab-pane fade' id='nav-quantity' role='tabpanel' aria-labelledby='nav-quantity-tab'>
+                        {changeQuantityOfBooks ? <ChangeQuantityOfBooks/> : <></>}
+                    </div>
+                    <div className='tab-pane fade' id='nav-messages' role='tabpanel' aria-labelledby='nav-messages-tab'>
+                        {messagesClick ? <AdminMessages/> : <></>}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
