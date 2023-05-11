f5c1fff390763f73cb93da46418ebb2c6f5733ec
Slackjaw1431 <55801287+Slackjaw1431@users.noreply.github.com>
2023-05-11 07:41:27 -0400

    Adjusting quantity and adding/deleting books

--- src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
+++ src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
@@ -1,104 +1,118 @@
-import React from "react";
 import { useEffect, useState } from "react";
-import BookModel from '../../../models/BookModel';
-import {ChangeQuantityOfBook} from "./ChangeQuantityOfBooks";
-import {Pagination} from "../../utils/Pagination";
-import SpinnerLoading from "../../utils/SpinnerLoading";
+import BookModel from "../../../models/BookModel";
+import { useOktaAuth } from '@okta/okta-react';
 
-export const ChangeQuantityOfBooks = () => {
+export const ChangeQuantityOfBook: React.FC<{ book: BookModel, deleteBook: any }> = (props, key) => {
 
-    const [books, setBooks] = useState<BookModel[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [httpError, setHttpError] = useState(null);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [booksPerPage] = useState(5);
-    const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
-    const [totalPages, setTotalPages] = useState(0);
-
-    const [bookDelete, setBookDelete] = useState(false);
+    const { authState } = useOktaAuth();
+    const [quantity, setQuantity] = useState<number>(0);
+    const [remaining, setRemaining] = useState<number>(0);
 
     useEffect(() => {
-        const fetchBooks = async () => {
-            const baseUrl: string = `http://localhost:8080/api/books?page=${currentPage - 1}&size=${booksPerPage}`;
-
-            const response = await fetch(baseUrl);
-
-            if (!response.ok) {
-                throw new Error('Something went wrong!');
-            }
-
-            const responseJson = await response.json();
-
-            const responseData = responseJson._embedded.books;
-
-            setTotalAmountOfBooks(responseJson.page.totalElements);
-            setTotalPages(responseJson.page.totalPages);
-
-            const loadedBooks: BookModel[] = [];
-
-            for (const key in responseData) {
-                loadedBooks.push({
-                    id: responseData[key].id,
-                    title: responseData[key].title,
-                    author: responseData[key].author,
-                    description: responseData[key].description,
-                    copies: responseData[key].copies,
-                    copiesAvailable: responseData[key].copiesAvailable,
-                    category: responseData[key].category,
-                    img: responseData[key].img,
-                });
-            }
-
-            setBooks(loadedBooks);
-            setIsLoading(false);
+        const fetchBookInState = () => {
+            props.book.copies ? setQuantity(props.book.copies) : setQuantity(0);
+            props.book.copiesAvailable ? setRemaining(props.book.copiesAvailable) : setRemaining(0);
         };
-        fetchBooks().catch((error: any) => {
-            setIsLoading(false);
-            setHttpError(error.message);
-        })
-    }, [currentPage, bookDelete]);
+        fetchBookInState();
+    }, []);
 
-    const indexOfLastBook: number = currentPage * booksPerPage;
-    const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
-    let lastItem = booksPerPage * currentPage <= totalAmountOfBooks ?
-        booksPerPage * currentPage : totalAmountOfBooks;
+    async function increaseQuantity() {
+        const url = `http://localhost:8080/api/admin/secure/increase/book/quantity/?bookId=${props.book?.id}`;
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        };
 
-    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
-
-    const deleteBook = () => setBookDelete(!bookDelete);
-
-    if (isLoading) {
-        return (
-            <SpinnerLoading/>
-        );
+        const quantityUpdateResponse = await fetch(url, requestOptions);
+        if (!quantityUpdateResponse.ok) {
+            throw new Error('Quantity increase wrong!');
+        }
+        setQuantity(quantity + 1);
+        setRemaining(remaining + 1);
     }
 
-    if (httpError) {
-        return (
-            <div className='container m-5'>
-                <p>{httpError}</p>
-            </div>
-        );
+    async function decreaseQuantity() {
+        const url = `http://localhost:8080/api/admin/secure/decrease/book/quantity/?bookId=${props.book?.id}`;
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const quantityUpdateResponse = await fetch(url, requestOptions);
+        if (!quantityUpdateResponse.ok) {
+            throw new Error('Quantity decrease error!');
+        }
+        setQuantity(quantity - 1);
+        setRemaining(remaining - 1);
+    }
+
+    async function deleteBook() {
+        const url = `http://localhost:8080/api/admin/secure/delete/book/?bookId=${props.book?.id}`;
+        const requestOptions = {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const updateResponse = await fetch(url, requestOptions);
+        if (!updateResponse.ok) {
+            throw new Error('Book deletion error!');
+        }
+        props.deleteBook();
     }
 
     return (
-        <div className='container mt-5'>
-            {totalAmountOfBooks > 0 ?
-                <>
-                    <div className='mt-3'>
-                        <h3>Number of results: ({totalAmountOfBooks})</h3>
+        <div className='card mt-3 shadow p-3 mb-3 bg-body rounded'>
+            <div className='row g-0'>
+                <div className='col-md-2'>
+                    <div className='d-none d-lg-block'>
+                        {props.book.img ?
+                            <img src={props.book.img} width='123' height='196' alt='Book' />
+                            :
+                            <img src={require('./../../../Images/BooksImages/book-luv2code-1000.png')}
+                                 width='123' height='196' alt='Book' />
+                        }
                     </div>
-                    <p>
-                        {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
-                    </p>
-                    {books.map(book => (
-                        <ChangeQuantityOfBook book={book} key={book.id} deleteBook={deleteBook}/>
-                    ))}
-                </>
-                :
-                <h5>Add a book before changing quantity</h5>
-            }
-            {totalPages > 1 && <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate}/>}
+                    <div className='d-lg-none d-flex justify-content-center align-items-center'>
+                        {props.book.img ?
+                            <img src={props.book.img} width='123' height='196' alt='Book' />
+                            :
+                            <img src={require('./../../../Images/BooksImages/book-luv2code-1000.png')}
+                                 width='123' height='196' alt='Book' />
+                        }
+                    </div>
+                </div>
+                <div className='col-md-6'>
+                    <div className='card-body'>
+                        <h5 className='card-title'>{props.book.author}</h5>
+                        <h4>{props.book.title}</h4>
+                        <p className='card-text'> {props.book.description} </p>
+                    </div>
+                </div>
+                <div className='mt-3 col-md-4'>
+                    <div className='d-flex justify-content-center algin-items-center'>
+                        <p>Total Quantity: <b>{quantity}</b></p>
+                    </div>
+                    <div className='d-flex justify-content-center align-items-center'>
+                        <p>Books Remaining: <b>{remaining}</b></p>
+                    </div>
+                </div>
+                <div className='mt-3 col-md-1'>
+                    <div className='d-flex justify-content-start'>
+                        <button className='m-1 btn btn-md btn-danger' onClick={deleteBook}>Delete</button>
+                    </div>
+                </div>
+                <button className='m1 btn btn-md main-color text-white' onClick={increaseQuantity}>Add Quantity</button>
+                <button className='m1 btn btn-md btn-warning' onClick={decreaseQuantity}>Decrease Quantity</button>
+            </div>
         </div>
     );
 }
