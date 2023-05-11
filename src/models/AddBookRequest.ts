2bd18c9c99bc86de552aa555754b14ba24e6e1d9
Slackjaw1431 <55801287+Slackjaw1431@users.noreply.github.com>
2023-05-10 23:52:35 -0400

    Adding new book

--- src/models/AddBookRequest.ts
+++ src/models/AddBookRequest.ts
@@ -6,12 +6,11 @@ class AddBookRequest{
     img?:string;
     copies: number;
 
-    constructor(title: string, author: string, description: string, category: string, img: string, copies: number) {
+    constructor(title: string, author: string, description: string, category: string, copies: number) {
         this.title = title;
         this.author = author;
         this.description = description;
         this.category = category;
-        this.img = img;
         this.copies = copies;
     }
 }
